3e0e4b1039fb335d51cfbfa31dcc7b59dcb05927
tablezaj <tablezaj@yahoo.com>
2023-03-09 10:41:17 -0800

    set up trial api

--- trial-api/src/index.js
+++ trial-api/src/index.js
@@ -1,1 +1,23 @@
 // trial-api main file
+ const express = require("express");
+ const app = express();
+ const port = 5000;
+
+
+ app.get("/", (req, res) => {
+      res.send(`Hello World! Current port is ${port}`);
+ });
+
+//  app.post(arg1, arg2);
+//  app.post("/hello")
+//  app.post("_____", functionNme);
+//  app.post("/hello", () => {});
+ app.post("/hello", (req, res) => {
+    console.log(`/hello endpoint was hit. Sending a response`);
+    res.send('Hello hello, my friend');
+    console.log(`Response send .....`);
+ });
+
+ app.listen(port, () => {
+    console.log(`Trial-API listening on port ${port} was started successfully`);
+ });
